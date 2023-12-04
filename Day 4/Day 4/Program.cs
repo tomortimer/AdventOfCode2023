aaa0b5a7a345b6db370c28251773fbc09f6385de
tomortimer <tcmortimer@outlook.com>
2023-12-04 20:24:21 +0000

    THIS TOOK so much bashing my head against the wall. could absolutely refactor this into just two for loops instead of relooping outside of first but im just happy it works

--- Day 4/Day 4/Program.cs
+++ Day 4/Day 4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using MorteTools;
 
 namespace Day_4
@@ -8,11 +9,14 @@ namespace Day_4
         static void Main(string[] args)
         {
             FileParser parser = new FileParser();
-            List<string> lines = parser.GetLinesFromTxtTrimmed("input.txt", 7);
+            //List<string> lines = parser.GetLinesFromTxtTrimmed("input.txt", 7);
+            List<string> lines = parser.GetLinesFromTxt("input.txt");
+            List<int> scoreDict = new List<int>();
             int sum = 0;
-            for(int i = 0; i < lines.Count(); i++) 
+            for (int i = 0; i < lines.Count(); i++) 
             {
-                double numMatches = -1;
+                int numMatches = 0;
+                int cardIndex = Convert.ToInt32(Regex.Match(lines[i], "[0-9]+").Groups[0].Value) - 1;
                 string line = lines[i].Replace("  ", " ");
                 string[] halves = line.Split('|');
                 string[] leftNums = halves[0].Substring(1).Split(" ");
@@ -24,7 +28,30 @@ namespace Day_4
                         numMatches++;
                     }
                 }
-                if(numMatches >= 0) { sum += (int)Math.Pow(2.0, numMatches); }
+                scoreDict.Add(numMatches);
+                /*if(numMatches > 0) 
+                {
+                    //sum += (int)Math.Pow(2.0, numMatches);
+                    for(int x = 1; x <= numMatches; x++)
+                    {
+                        lines.Add(lines[cardIndex + x]);
+                        Console.WriteLine("Card " + (cardIndex+1) + " adds Card " + (cardIndex + x+ 1));
+                    }
+                }*/
+            }
+            //Console.WriteLine(sum)
+            List<int> copies = new List<int>();
+            for(int i = 0; i < lines.Count();  i++)
+            {
+                copies.Add(1);
+            }
+            for(int i = 0; i < lines.Count(); i++)
+            {
+                for(int y = 0; y < scoreDict[i]; y++)
+                {
+                    copies[i + y + 1] += copies[i];
+                }
+                sum += copies[i];
             }
             Console.WriteLine(sum);
         }

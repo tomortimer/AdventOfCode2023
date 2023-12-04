771f5fed947c17f939b4b5dcda5f0e84ef068aef
tomortimer <tcmortimer@outlook.com>
2023-12-04 19:19:06 +0000

    part 1 solution; just lots of string manip and some powers

---
+++ Day 4/Day 4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MorteTools;
+
+namespace Day_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileParser parser = new FileParser();
+            List<string> lines = parser.GetLinesFromTxtTrimmed("input.txt", 7);
+            int sum = 0;
+            for(int i = 0; i < lines.Count(); i++) 
+            {
+                double numMatches = -1;
+                string line = lines[i].Replace("  ", " ");
+                string[] halves = line.Split('|');
+                string[] leftNums = halves[0].Substring(1).Split(" ");
+                List<string> winningNums = new List<string>(halves[1].Substring(1).Split(" "));
+                foreach(string num in leftNums)
+                {
+                    if (winningNums.Contains(num))
+                    {
+                        numMatches++;
+                    }
+                }
+                if(numMatches >= 0) { sum += (int)Math.Pow(2.0, numMatches); }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
--- MorteTools/MorteTools/FileParser.cs
+++ MorteTools/MorteTools/FileParser.cs
@@ -26,5 +26,20 @@
             reader.Close();
             return lines;
         }
+
+        public List<string> GetLinesFromTxtTrimmed(string filename, int startIndex)
+        {
+            StreamReader reader = new StreamReader(filename);
+            List<string> lines = new List<string>();
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+
+                lines.Add(line.Substring(startIndex));
+                line = reader.ReadLine();
+            }
+            reader.Close();
+            return lines;
+        }
     }
 }

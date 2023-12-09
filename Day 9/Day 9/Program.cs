1e6438f8f93c46e1b1efdbd60bb7322af46b47b7
tomortimer <tcmortimer@outlook.com>
2023-12-09 22:19:23 +0000

    part 1 done with a neat little recursive sequence object

---
+++ Day 9/Day 9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MorteTools;
+
+namespace Day_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileParser fp = new FileParser();
+            List<string> lines = fp.GetLinesFromTxt("input.txt");
+            int sum = 0;
+            foreach(string line in lines)
+            {
+                string[] arr = line.Split(' ');
+                List<int> lineNums = new List<int>();
+                for(int i = 0; i < arr.Length; i++)
+                {
+                    lineNums.Add(int.Parse(arr[i]));
+                }
+                Sequence tmp = new Sequence(lineNums);
+                sum += tmp.Extrapolate();
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Day 9/Day 9/Sequence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MorteTools;
+namespace Day_9
+{
+    internal class Sequence
+    {
+        List<int> numbers;
+        Sequence differences;
+        public Sequence(List<int> input)
+        {
+            numbers = [.. input];
+            // now calculate differences
+            List<int> diffs = new List<int>();
+            int zeroCtr = 0;
+            for (int i = 0; i < numbers.Count()-1; i++)
+            {
+                int tmp = numbers[i + 1] - numbers[i];
+                diffs.Add(tmp);
+                if(tmp == 0) { zeroCtr++; }
+            }
+            if (!(zeroCtr == diffs.Count())) 
+            {
+                differences = new Sequence(diffs);
+            }
+            else { differences = null; }
+        }
+
+        public int Extrapolate()
+        {
+            int newTerm = 0;
+            if(differences != null)
+            {
+                newTerm = numbers[numbers.Count()-1] + differences.Extrapolate();
+            }
+            else { newTerm = numbers[numbers.Count() - 1]; }
+            return newTerm;
+        }
+    }
+}

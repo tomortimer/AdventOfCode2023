98946451b5c8b24de0146b4e5fa078bfd95de469
tomortimer <tcmortimer@outlook.com>
2023-12-13 15:59:04 +0000

    idk why but this doesn't work

--- Day 10/Day 10/Program.cs
+++ Day 10/Day 10/Program.cs
@@ -73,15 +73,19 @@ namespace Day_10
             Console.WriteLine((double)visited.Count / 2f);
             Console.WriteLine("Start: "+startIndex.Item1+","+startIndex.Item2);
             Console.WriteLine("Start Type: " + pipes[startIndex.Item1, startIndex.Item2]);
-            Console.WriteLine("End: "+endNode.Item1+","+endNode.Item2);
+            Console.WriteLine("End: "+endNode.Item1+","+endNode.Item2);*/
 
-            
+
             StreamWriter writer = new StreamWriter("output.txt");
             for (int y = 0; y < yBound; y++)
             {
                 for (int x = 0; x < xBound; x++)
                 {
-                    if (visited.Contains(new Tuple<int, int>(x, y))) { writer.Write(Convert.ToString(lines[y][x])); }
+                    if (visited.Contains(new Tuple<int, int>(x, y))) 
+                    {
+                        if (lines[y][x] == 'S') { writer.Write(pipes[x, y].ToString()); }
+                        else { writer.Write(Convert.ToString(lines[y][x])); }
+                    }
                     else
                     {
                         writer.Write(".");
@@ -90,13 +94,36 @@ namespace Day_10
                 writer.WriteLine();
             }
             writer.Close();
-            //this looks super cool just needed an idea of the puzzle*/
+            //this looks super cool just needed an idea of the puzzle
 
             //part two stuff
             //need to have a left hand and right hand side of path somehow??
             //selective flooding maybe?
             // ideq: expand grid, like zoom in so each symbol is now 3x3 then flood from a chosen point, should work in theory. as long as chosen point is right. then count the number of flooded blank 3x3s?
-            
+            lines = fp.GetLinesFromTxt("output.txt");
+            Pipe[,] expandedPipes = new Pipe[lines[0].Length*3, lines.Count() * 3];
+            int yPos = 0; int xPos = 0;
+            for(int y = 0; y < lines.Count(); y++)
+            {
+                for(int x = 0;x < lines[0].Length; x++)
+                {
+                    expandedPipes[xPos, yPos] = new Pipe('.');
+                    if (lines[y][x] == 'L' || lines[y][x] == '|' || lines[y][x] == 'J') { expandedPipes[xPos + 1, yPos] = new Pipe('|'); }
+                    else { expandedPipes[xPos + 1, yPos] = new Pipe('.'); }
+                    expandedPipes[xPos+2, yPos] = new Pipe('.');
+                    if (lines[y][x] == '-' || lines[y][x] == '7' || lines[y][x] == 'J') { expandedPipes[xPos, yPos+1] = new Pipe('-'); }
+                    else { expandedPipes[xPos, yPos+1] = new Pipe('.'); }
+                    expandedPipes[xPos+1, yPos+1] = new Pipe(lines[y][x]);
+                    if (lines[y][x] == 'F' || lines[y][x] == '-' || lines[y][x] == 'L') { expandedPipes[xPos + 2, yPos+1] = new Pipe('-'); }
+                    else { expandedPipes[xPos + 2, yPos+1] = new Pipe('.'); }
+                    expandedPipes[xPos, yPos + 2] = new Pipe('.');
+                    if (lines[y][x] == '7' || lines[y][x] == '|' || lines[y][x] == 'F') { expandedPipes[xPos + 1, yPos+2] = new Pipe('|'); }
+                    else { expandedPipes[xPos + 1, yPos+2] = new Pipe('.'); }
+                    expandedPipes[xPos + 2, yPos + 2] = new Pipe('.');
+                    xPos += 3;
+                }
+                yPos += 3;
+            }
         }
 
         static bool InBounds(Tuple<int,int> pos, int xBound, int yBound)

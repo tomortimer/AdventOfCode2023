f4f71b502d8f6392611e4c12f2a197486dd849bc
tomortimer <tcmortimer@outlook.com>
2023-12-16 18:39:45 +0000

    skipped day 12 bcs my head hertz, just started on th9is minimal progress for now

---
+++ Day 13/Day 13/Map.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_13
+{
+    internal class Map
+    {
+        char[,] map;
+        public Map(MorteTools.List<string> inp)
+        {
+            for(int y = 0; y < inp.Count(); y++)
+            {
+                for(int x = 0; x < inp[y].Length; x++)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ Day 13/Day 13/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Day_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MorteTools.FileParser fp = new MorteTools.FileParser();
+            List<Map> maps = new List<Map>();
+        }
+    }
+}

57e63125ded5343fac6951208bfe7b872abc64ac
tomortimer <tcmortimer@outlook.com>
2023-12-18 22:00:16 +0000

    was stupid and forgot to validate size of list in box, ezpz after that though nw

---
+++ Day 15/Day 15/Box.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_15
+{
+    internal class Box
+    {
+        List<int> focalLengths;
+        List<string> lenses;
+
+        public Box()
+        {
+            focalLengths = new List<int>();
+            lenses = new List<string>();
+        }
+
+        public void RemoveFront(string inp)
+        {
+            if(lenses.Contains(inp))
+            {
+                int tmp = lenses.IndexOf(inp);
+                lenses.RemoveAt(tmp);
+                focalLengths.RemoveAt(tmp);
+            }
+            
+        }
+
+        public void AddLens(string inp)
+        {
+            if (lenses.Count() < 9)
+            {
+
+
+                string[] parts = inp.Split('=');
+                string lens = parts[0];
+                int focalDistance = int.Parse(parts[1]);
+                if (lenses.Contains(lens))
+                {
+                    int tmp = lenses.IndexOf(lens);
+                    focalLengths[tmp] = focalDistance;
+                }
+                else
+                {
+                    focalLengths.Add(focalDistance);
+                    lenses.Add(lens);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string ret = "";
+            for(int i = 0; i < lenses.Count; i++)
+            {
+                ret+= lenses[i] + " " + focalLengths[i];
+            }
+            return ret;
+        }
+
+        public int GetBoxPower()
+        {
+            int score = 0;
+            for(int i = 0; i < focalLengths.Count(); i++) 
+            {
+                int val = (i+1) * focalLengths[i];
+                score += val;
+            }
+            return score;
+        }
+    }
+}
--- Day 15/Day 15/Program.cs
+++ Day 15/Day 15/Program.cs
@@ -9,12 +9,38 @@ namespace Day_15
             FileParser fp = new FileParser();
             string input = fp.GetLinesFromTxt("input.txt")[0];
             string[] parts = input.Split(',');
-            int sum = 0;
+            //int sum = 0;
+            Box[] boxes = new Box[256];
+            for(int i = 0; i < boxes.Length; i++)
+            {
+                boxes[i] = new Box();
+            }
             foreach (string part in parts)
             {
-                sum += Hash(part);
+                if (part.Contains('='))
+                {
+                    string half = part.Split('=')[0];
+                    int hash = Hash(half);
+                    boxes[hash].AddLens(part);
+                }
+                else
+                {
+                    string half = part.Split('-')[0];
+                    int hash = Hash(half);
+                    boxes[hash].RemoveFront(half);
+                }
+                //sum += hash;
             }
-            Console.WriteLine("Part One: "+sum);
+            //Console.WriteLine("Part One: "+sum);
+            int total = 0;
+            for(int i = 0; i < boxes.Length; i++) 
+            {
+                int boxValue = boxes[i].GetBoxPower();
+                int val = (i + 1) * boxValue;
+                Console.WriteLine("Box " + i + ": " + (i + 1) + " * " + boxValue + " = " + val);
+                total += val;
+            }
+            Console.WriteLine("Part Two: " + total);
         }
 
         static int Hash(string inp)

daabb1f6afe3f00ae48175e45649815c00182085
tomortimer <tcmortimer@outlook.com>
2023-12-18 18:03:27 +0000

    memoisation headache probably not going to pt2

--- Day 12/Day 12/Program.cs
+++ Day 12/Day 12/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using MorteTools;
 namespace Day_12
 {
@@ -10,22 +11,42 @@ namespace Day_12
         {
             FileParser fp = new FileParser();
             List<string> lines = fp.GetLinesFromTxt("input.txt");
+            Memoiser memoMachine = new Memoiser();
+
+            Func<Tuple<string,string>, int> GetPatterns = GetPossiblePatterns;
+            GetPatterns = memoMachine.Memoise(GetPatterns);
+            Func<Tuple<string,string>, int> HashCase = Hash;
+            HashCase = memoMachine.Memoise(HashCase);
+            Func<Tuple<string,string>, int> DotCase = Dot;
+            DotCase = memoMachine.Memoise(DotCase);
 
             int sum = 0;
             foreach (string line in lines)
             {
                 string pattern = line.Split(' ')[0];
-                List<int> groups = fp.GetListInt(line.Split(' ')[1], ',');
-                int tmp = GetPossiblePatterns(pattern, groups);
+                string groups = line.Split(' ')[1];
+                string newPattern = "";
+                string newGroups = "";
+                for(int i = 0; i < 5; i++)
+                {
+                    newPattern += pattern;
+                    if(i != 4) { newPattern += "?"; }
+                    newGroups += groups;
+                    if(i != 4) { newGroups += ","; }
+                }
+                int tmp = GetPatterns(new Tuple<string,string>(newPattern, newGroups));
                 Console.WriteLine(line+ " has " +tmp+ " arrangements");
                 sum += tmp;
             }
             Console.WriteLine("Part One: "+sum);
         }
 
-        static int GetPossiblePatterns(string pattern, List<int> groups) 
+        static int GetPossiblePatterns(Tuple<string,string> inp) 
         {
             int count = 0;
+            //reconstruct list and pattern
+            string pattern = inp.Item1;
+            List<int> groups = ParseInts(inp.Item2);
             
             //base cases for empty groups lists or empty pattern
             if(groups.Count() == 0)
@@ -41,26 +62,31 @@ namespace Day_12
             //move until get to hash
             if(currentChar == '.')
             {
-                count = Dot(pattern, groups.Clone());
+                count = Dot(inp);
             }else if(currentChar == '#') 
             {
-                count = Hash(pattern, groups.Clone());
+                count = Hash(inp);
             }
             else if(currentChar == '?')
             {
-                count = Hash(pattern, groups.Clone()) + Dot(pattern, groups);
+                count = Hash(inp) + Dot(inp);
             }
 
             return count;
         }
 
-        static int Dot(string pattern, List<int> groups)
+        static int Dot(Tuple<string,string> inp)
         {
-            return GetPossiblePatterns(pattern.Substring(1), groups);
+            string pattern = inp.Item1.Substring(1);
+            return GetPossiblePatterns(new Tuple<string,string>(pattern, inp.Item2));
         }
 
-        static int Hash(string pattern, List<int> groups)
+        static int Hash(Tuple<string,string> inp)
         {
+            //reconstruct list and pattern
+            string pattern = inp.Item1;
+            List<int> groups = ParseInts(inp.Item2);
+
             int currentGroup = groups[0];
             string testGroup;
             try
@@ -88,7 +114,8 @@ namespace Day_12
             if (pattern[currentGroup] == '.' || pattern[currentGroup] == '?')
             {
                 groups.RemoveAt(0);
-                return GetPossiblePatterns(pattern.Substring(currentGroup+1), groups);
+                pattern = pattern.Substring(currentGroup + 1);
+                return GetPossiblePatterns(new Tuple<string, string>(pattern, groups.ToString()));
             }
 
             //otherwise cry
@@ -101,6 +128,20 @@ namespace Day_12
             for(int i = 0; i < num; i++)
             {
                 ret += inp.ToString();
+            }
+            return ret;
+        }
+
+        static List<int> ParseInts(string input)
+        {
+            string[] parts = input.Split(',');
+            List<int> ret = new List<int>();
+            if (parts[0] != "")
+            {
+                foreach (string part in parts)
+                {
+                    ret.Add(int.Parse(part));
+                }
             }
             return ret;
         }
---
+++ MorteTools/MorteTools/Memoiser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace MorteTools
+{
+    public class Memoiser
+    {
+        public Func<R> Memoise<R>(Func<R> func)
+        {
+            object cache = null;
+            return () =>
+            {
+                if (cache == null)
+                    cache = func();
+                return (R)cache;
+            };
+        }
+
+        public Func<A, R> Memoise<A, R>(Func<A, R> func)
+        {
+            var cache = new System.Collections.Generic.Dictionary<A, R>();
+            return a =>
+            {
+                if (cache.TryGetValue(a, out R value))
+                    return value;
+                value = func(a);
+                cache.Add(a, value);
+                return value;
+            };
+        }
+
+        public Func<A, R> ThreadSafeMemoise<A, R>(Func<A, R> func)
+        {
+            var cache = new ConcurrentDictionary<A, R>();
+            return argument => cache.GetOrAdd(argument, func);
+        }
+    }
+}

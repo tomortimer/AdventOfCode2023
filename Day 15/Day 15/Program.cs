7035d50820245d219e43703eaa260f741690120e
tomortimer <tcmortimer@outlook.com>
2023-12-18 21:24:22 +0000

    part one day 15 complete

---
+++ Day 15/Day 15/Program.cs
@@ -0,0 +1,32 @@
+﻿using MorteTools;
+using System.Windows.Markup;
+namespace Day_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileParser fp = new FileParser();
+            string input = fp.GetLinesFromTxt("input.txt")[0];
+            string[] parts = input.Split(',');
+            int sum = 0;
+            foreach (string part in parts)
+            {
+                sum += Hash(part);
+            }
+            Console.WriteLine("Part One: "+sum);
+        }
+
+        static int Hash(string inp)
+        {
+            int currentValue = 0;
+            foreach(char c in inp)
+            {
+                currentValue += c;
+                currentValue *= 17;
+                currentValue %= 256;
+            }
+            return currentValue;
+        }
+    }
+}

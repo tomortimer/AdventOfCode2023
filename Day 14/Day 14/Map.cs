6a452e2b0dd36bbfc3a9be4b99bf421cddafe20a
tomortimer <tcmortimer@outlook.com>
2023-12-18 20:57:41 +0000

    DONE¬!!! required manually finding the period of the cycle but otherwise fine, i'm happy with this one :)

---
+++ Day 14/Day 14/Map.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using MorteTools;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_14
+{
+    internal class Map
+    {
+        char[,] map;
+        int width;
+        int height;
+
+        public Map(List<string> inp) 
+        {
+            width = inp[0].Length;
+            height = inp.Count();
+            map = new char[width, height];
+            for(int y = 0; y < height; y++)
+            {
+                for(int x = 0; x < width; x++)
+                {
+                    map[x, y] = inp[y][x];
+                }
+            }
+        }
+        public int CalcScore()
+        {
+            int score = 0;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (map[x, y] == 'O') { score += (height - y); }
+                }
+            }
+            return score;
+        }
+
+        public void RollAllNorth()
+        {
+            for(int y = 0; y < height; y++)
+            {
+                for(int x = 0; x < width; x++)
+                {
+                    if (map[x, y] == 'O') { RollRockNorth(x, y); }
+                }
+            }
+        }
+
+        private void RollRockNorth(int x, int y)
+        {
+            while(y > 0 && map[x, y - 1] == '.')
+            {
+                map[x, y] = '.';
+                y--;
+                map[x, y] = 'O';
+            }
+        }
+        public void RollAllWest()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (map[x, y] == 'O') { RollRockWest(x, y); }
+                }
+            }
+        }
+
+        private void RollRockWest(int x, int y)
+        {
+            while (x > 0 && map[x - 1, y] == '.')
+            {
+                map[x, y] = '.';
+                x--;
+                map[x, y] = 'O';
+            }
+        }
+
+        public void RollAllSouth()
+        {
+            for (int y = height - 1; y >= 0; y--)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (map[x, y] == 'O') { RollRockSouth(x, y); }
+                }
+            }
+        }
+
+        private void RollRockSouth(int x, int y)
+        {
+            while (y < height-1 && map[x, y +1] == '.')
+            {
+                map[x, y] = '.';
+                y++;
+                map[x, y] = 'O';
+            }
+        }
+
+        public void RollAllEast()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = width-1; x >= 0; x--)
+                {
+                    if (map[x, y] == 'O') { RollRockEast(x, y); }
+                }
+            }
+        }
+
+        private void RollRockEast(int x, int y)
+        {
+            while (x < width-1 && map[x+1, y] == '.')
+            {
+                map[x, y] = '.';
+                x++;
+                map[x, y] = 'O';
+            }
+        }
+
+        public void SpinCycle()
+        {
+            RollAllNorth();
+            RollAllWest();
+            RollAllSouth();
+            RollAllEast();
+        }
+
+        public void PrintGrid()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.Write(map[x, y]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public override string ToString()
+        {
+            string ret = "";
+            for(int y = 0; y < height; y++)
+            {
+                for(int x = 0; x < width; x++)
+                {
+                    ret += map[x, y];
+                }
+            }
+            return ret;
+        }
+    }
+}
---
+++ Day 14/Day 14/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MorteTools;
+namespace Day_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileParser fp = new FileParser();
+            List<string> lines = fp.GetLinesFromTxt("input.txt");
+            Map grid = new Map(lines);
+            /*Console.WriteLine("Before:");
+            grid.PrintGrid();*/
+            grid.RollAllNorth();
+            /*Console.WriteLine("After:");
+            grid.PrintGrid();*/
+            Console.WriteLine("Part One: " + grid.CalcScore());
+            System.Collections.Generic.List<string> results = new System.Collections.Generic.List<string>();
+            int ctr = 0;
+            string tmp = "";
+            bool cont = true;
+            int firstInstance = 0;
+            //CALCULATED CYCLE PERIOD OF 59
+            int offset = (1000000000 % 59);
+            //go until first repetition
+            do
+            {
+                results.Add(tmp);
+                grid.SpinCycle();
+                ctr++;
+                //Console.WriteLine("Cycle: " + ctr);
+                if((ctr - offset) % 59 == 0) { Console.WriteLine(grid.CalcScore()); }
+                tmp = grid.ToString();
+                if(results.Contains(tmp) && 1000000000 % (ctr+1) == 0) 
+                { 
+                    //cont = false;
+                    firstInstance = results.IndexOf(tmp);
+                }
+            } while (ctr < 1000000000);
+            Console.WriteLine("Part Two: " + grid.CalcScore());
+            Console.WriteLine("First occurence: " + firstInstance);
+            Console.WriteLine("Secon occurence: 199");
+            Console.WriteLine("Period = "+(199 - firstInstance));
+            //grid.PrintGrid();
+        }
+    }
+}

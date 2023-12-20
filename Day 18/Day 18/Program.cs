a3a1e2454e3c51f3345e839620858b91ca553de4
tomortimer <tcmortimer@outlook.com>
2023-12-20 17:39:04 +0000

    had to painfully convert a lot of values to longs to preserve accuracy, learnt a variation on area of a polygon of n vertices though so that was cool :)

--- Day 18/Day 18/Program.cs
+++ Day 18/Day 18/Program.cs
@@ -8,31 +8,65 @@ namespace Day_18
         {
             MorteTools.List<string> lines = new MorteTools.FileParser().GetLinesFromTxt("input.txt");
             Grid lagoon = new Grid();
-            Vector2 current = new Vector2(0,0);
-            Dictionary<string, Vector2> transformations = new Dictionary<string, Vector2> 
+            Tuple<long, long> current = new Tuple<long, long>(0,0);
+            Dictionary<string, Tuple<long, long>> transformations = new Dictionary<string, Tuple<long, long>> 
             {
-                {"R", new Vector2(1,0) },
-                {"U", new Vector2(0,-1)},
-                {"D", new Vector2(0,1)},
-                {"L", new Vector2(-1,0)}
+                {"R", new Tuple<long, long>(1,0) },
+                {"U", new Tuple < long, long >(0,-1)},
+                {"D", new Tuple < long, long >(0,1)},
+                {"L", new Tuple < long, long >(-1,0)}
             };
+            List<Tuple<long, long>> vertices = new List<Tuple<long,long>>();
+            long perimeter = 0;
             foreach(string line in lines)
             {
-                string direction = line.Split(' ')[0];
-                int moves = int.Parse(line.Split(' ')[1]);
-                for(int i = 0;  i < moves; i++)
+                vertices.Add(current);
+                /*string direction = line.Split(' ')[0];
+                int moves = Convert.ToInt32(line.Split(' ')[1]);*/
+                long moves = Convert.ToInt32(line.Split('#')[1].Substring(0, 5), 16);
+                int dirInt = Convert.ToInt32(line.Split('#')[1].Trim(')').Last().ToString(), 16);
+                string direction = "";
+                switch (dirInt)
+                {
+                    case 0: direction = "R"; break;
+                    case 1: direction = "D"; break;
+                    case 2: direction = "L"; break;
+                    case 3: direction = "U"; break;
+                }
+                current = new Tuple<long,long>(current.Item1 + transformations[direction].Item1 * moves, current.Item2 + transformations[direction].Item2 * moves);
+                perimeter += moves;
+                //offset for enabling part 2
+                /*for(int i = 0;  i < moves; i++)
                 {
                     current += transformations[direction];
                     current = lagoon.AddChar((int)current.X, (int)current.Y, '#');
-                }
+                }*/
+                
             }
-            //lagoon.PrintGrid();
-            //Console.WriteLine("----------------------");
-            Vector2 tmp = lagoon.GetStartCoord();
+            /*Vector2 tmp = lagoon.GetStartCoord();
             tmp = new Vector2(tmp.X + 1, tmp.Y + 1);
             lagoon.Flood(tmp);
             //lagoon.PrintGrid();
-            Console.WriteLine(lagoon.CountChar('#'));
+            Console.WriteLine(lagoon.CountChar('#'));*/
+            long area = AreaFromVertices(vertices);
+            area += perimeter / 2 + 1;
+            Console.WriteLine(area);
+        }
+
+        static long AreaFromVertices(List<Tuple<long, long>> vertices)
+        {
+            long area = 0;
+            for(int i = 0; i < vertices.Count; i++) 
+            {
+                int next = (i + 1) % vertices.Count;
+                int last = i - 1;
+                if(last < 0) { last = vertices.Count - 1; }
+                //using shoelace adaptation of area of any polygon
+                long value = (long)(vertices[i].Item2 * (vertices[next].Item1 - vertices[last].Item1));
+                area += value;
+            }
+            area = Math.Abs(area) / 2;
+            return area;
         }
     }
 }
